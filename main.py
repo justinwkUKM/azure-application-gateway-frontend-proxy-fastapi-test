8fd72ac595b234a2db1a259f2b3de2136ff21515
Waqas Khalid Obeidy <waqasobeidy@gmail.com>
2024-11-16 23:54:35 +0800

    added new route

--- main.py
+++ main.py
@@ -35,6 +35,30 @@ def proxy_request(url: str):
     except Exception as e:
         return {"error": str(e)}
     
+@app.post("/proxy_two_urls")
+def proxy_request2(first_url: str, second_url: str):
+    """
+    1. Makes a GET request to the first_url
+    2. Takes that response and sends it as a body parameter in a POST request to second_url
+    """
+    try:
+        print(first_url)
+        print(second_url)
+        # Second POST request using first response as body
+        response = requests.post(first_url, params={"url": second_url})
+        print(response.status_code)
+        data = response.json()
+        
+        
+        return {
+            
+                "status_code": response.status_code,
+                "body": data["result"]
+            
+        }
+    except Exception as e:
+        return {"error": str(e)}
+    
 @app.get('/')
 async def root():
     return {"message": "Welcome to Frontend Proxy Test Server"}
